6ff9c5b6dc06be95e2daed8679f5026f47c45115
Amy Westcott <amy.westcott@yahoo.com>
2020-03-08 23:25:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(righttNumbers, 10);
-
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`;
-  }
+  let leftNumbers = dodger.style.left.replace("px", "");
+ let left = parseInt(leftNumbers, 10);
+ 
+ if (left < 360) {
+       dodger.style.left = `${left + 10}px`;
+ }    
 }
